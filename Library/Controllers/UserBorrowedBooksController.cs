970a1aaafb5f55661688f779cd5ff48f3a16b3ac
Muhammed Mehdi Ay <aymuhammedmehdi@gmail.com>
2022-05-23 13:39:51 +0300

    User için ödünç alınan kitaplar raporu oluşturuldu

--- Library/Controllers/UserBorrowedBooksController.cs
+++ Library/Controllers/UserBorrowedBooksController.cs
@@ -1,6 +1,7 @@
 ﻿using Library.DTO;
 using Library.Helpers;
 using Library.Service;
+using Rotativa;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,28 +21,50 @@ namespace Library.Controllers
 
 
             var books = userBorrowedBooksService.GetUserAllBorrowedBooks(validUserID);
-            
+
             var data = from c in books
-                                  select new BorrowedBookListForDiplayDTO
-                                  {
-                                      BookName=c.BookName,
-                                      AuthorName=c.AuthorName,
-                                      Barcode=c.Barcode,
-                                      BookBorrowFee=c.BookBorrowFee,
+                       select new BorrowedBookListForDiplayDTO
+                       {
+                           BookName = c.BookName,
+                           AuthorName = c.AuthorName,
+                           Barcode = c.Barcode,
+                           BookBorrowFee = c.BookBorrowFee,
 
-                                      BorrrowDate =c.BorrrowDate.ToString("dd MM yyyy"),
-                                      BorrowExpiresDate=c.BorrowExpiresDate.ToString("dd MM yyyy"),
-                                      CampusName=c.CampusName,
-                                      IsReturnedBool=c.IsReturned,
-                                      IsReturned=Utility.GetIsReturned(c.IsReturned),
-                                  };
+                           BorrrowDate = c.BorrrowDate.ToString("dd MM yyyy"),
+                           BorrowExpiresDate = c.BorrowExpiresDate.ToString("dd MM yyyy"),
+                           CampusName = c.CampusName,
+                           IsReturnedBool = c.IsReturned,
+                           IsReturned = Utility.GetIsReturned(c.IsReturned),
+                       };
 
 
 
-            
+
             return View(data);
         }
-        
-        
+        public ActionResult Report()
+        {
+            var validUser = Helpers.Utility.GetValidUserInfo();
+            var borrowedBooks = userBorrowedBooksService.GetUserAllBorrowedBooks(validUser.UserID).ToList();
+            return PDF(borrowedBooks, "Report");
+        }
+        public ViewAsPdf PDF(List<BorrowedBooksListDTO> data, string viewName)
+        {
+            var validUserId = Helpers.Utility.GetValidUserID();
+            var unix = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            return new ViewAsPdf(viewName, data)
+            {
+                FileName = string.Format("{0}-{1}.pdf", validUserId, unix),
+                ContentDisposition = Rotativa.Options.ContentDisposition.Inline,
+                PageSize = Rotativa.Options.Size.A6,
+                //PageWidth = 595,
+                //PageHeight = 1000,
+                PageOrientation = Rotativa.Options.Orientation.Portrait,
+                PageMargins = { Left = 0, Right = 0, Top = 0, Bottom = 0 },
+                CustomSwitches = "--no-stop-slow-scripts --javascript-delay 1000",
+            };
+        }
+
+
     }
 }
